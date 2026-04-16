5d3234bb20f5eb9d29a76224beac764b6cc07d69
quagrain <victorquagraine9@gmail.com>
2026-04-16 21:02:56 +0000

    Update chips and image

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -1,5 +1,5 @@
 <script>
-  import searchResultsImg from "$lib/assets/search-results.png";
+  import searchResultsImg from "$lib/assets/search-results.jpeg";
 
   /** @type {{ title: string, description: string, image: string }} */
   let { title, description, image } = $props();
@@ -14,12 +14,12 @@
     {#if image === "bubbles"}
       <div class="bubbles-graphic">
         <span class="bubble b1">Mechanics</span>
-        <span class="bubble b2">Read blog</span>
-        <span class="bubble b3">Surprise</span>
-        <span class="bubble b4">Direct Deposit</span>
-        <span class="bubble b5">Finance</span>
-        <span class="bubble b6">QuikPay money</span>
-        <span class="bubble b7">Blond</span>
+        <span class="bubble b2">Carpenters</span>
+        <span class="bubble b3">Electricians</span>
+        <span class="bubble b4">Plumbers</span>
+        <span class="bubble b5">Painters</span>
+        <span class="bubble b6">Cleaners</span>
+        <span class="bubble b7">Gardners</span>
       </div>
     {:else if image === "artisans"}
       <div class="artisans-collage">
@@ -100,7 +100,7 @@
 
   .card-image {
     flex: 1;
-    min-height: 260px;
+    min-height: 200px;
     display: flex;
     align-items: flex-end;
     justify-content: center;
@@ -111,21 +111,21 @@
   .bubbles-graphic {
     position: relative;
     width: 100%;
-    height: 180px;
+    height: 200px;
   }
 
   .bubble {
     position: absolute;
-    padding: 8px 16px;
+    padding: 10px 24px;
     border-radius: 50px;
-    font-size: var(--fs-xs);
+    font-size: var(--fs-sm);
     font-weight: 500;
     white-space: nowrap;
   }
 
   .b1 {
     background: #f3e8b5;
-    top: 10px;
+    top: -10%;
     left: 20%;
     transform: rotate(-5deg);
     color: #000;
@@ -133,13 +133,13 @@
   .b2 {
     background: #ade36d;
     top: 70px;
-    left: 5%;
+    left: 0;
     transform: rotate(-28deg);
     color: #000;
   }
   .b3 {
     background: #e6ccb8;
-    top: 60px;
+    top: 50px;
     left: 35%;
     transform: rotate(-35deg);
     color: #000;
@@ -147,28 +147,28 @@
   .b4 {
     background: #f2b8c6;
     top: 30px;
-    right: 15%;
+    right: -5%;
     transform: rotate(45deg);
     color: #000;
   }
   .b5 {
     background: #c8c2e6;
     bottom: 25px;
-    left: 14%;
+    left:0;
     transform: rotate(-15deg);
     color: #000;
   }
   .b6 {
     background: #cec4ff;
     bottom: 35px;
-    right: 12%;
+    right: -5%;
     transform: rotate(24deg);
     color: #000;
   }
   .b7 {
     background: #e6d5d2;
-    bottom: 15px;
-    left: 38%;
+    bottom: 28px;
+    left: 34%;
     transform: rotate(0deg);
     color: #000;
   }
@@ -222,7 +222,7 @@
   /* App screenshot */
   .app-screenshot {
     width: 90%;
-    height: 360px;
+    height: 300px;
     display: flex;
     justify-content: center;
     align-items: flex-start;
